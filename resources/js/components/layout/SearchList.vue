2ab0f0e6abd45b377612bcf0e3056aec7ec82ac4
libe9072 <libe90@nate.com>
2022-01-14 09:38:14 +0900

    검색목록

--- resources/js/components/layout/SearchList.vue
+++ resources/js/components/layout/SearchList.vue
@@ -15,7 +15,14 @@
 						>
 						<v-col cols="auto text-right">
 							<v-row no-gutters>
-								<v-col cols="12"> 김과장 (34M) </v-col>
+								<v-col cols="12">
+									{{ $store.state.sessionData.SNAME }} ({{
+										$store.state.sessionData.SADMIN === "Y"
+											? "관리자"
+											: $store.state.sessionData.SUCP +
+											  "M"
+									}})
+								</v-col>
 								<v-col>
 									<v-btn
 										small
@@ -28,6 +35,10 @@
 									<v-btn
 										small
 										color="info"
+										v-if="
+											$store.state.sessionData.SADMIN ===
+											'Y'
+										"
 										@click="
 											showMemberManagementLayer = true
 										"
@@ -197,19 +208,18 @@
 			</v-row>
 			<PasswordChangeLayer
 				:showPasswordChangeLayer.sync="showPasswordChangeLayer"
-				:imgData="imgData"
+				@snackData="setSnackData"
 			/>
-			<BosangLayer001
-				:showBosangLayer001.sync="showBosangLayer001"
-				:imgData="imgData"
-			/>
-			<BosangLayer002
-				:showBosangLayer002.sync="showBosangLayer002"
-				:imgData="imgData"
-			/>
+			<BosangLayer001 :showBosangLayer001.sync="showBosangLayer001" />
+			<BosangLayer002 :showBosangLayer002.sync="showBosangLayer002" />
 			<MemberManagementLayer
 				:showMemberManagementLayer.sync="showMemberManagementLayer"
-				:imgData="imgData"
+				@snackData="setSnackData"
+			/>
+			<SnackBar
+				:visible="showSnackBar"
+				:sid="snackData"
+				@close="showSnackBar = false"
 			/>
 		</v-form>
 	</v-card>
@@ -220,31 +230,23 @@ import PasswordChangeLayer from "../layer/PasswordChangeLayer";
 import BosangLayer001 from "../layer/BosangLayer001";
 import BosangLayer002 from "../layer/BosangLayer002";
 import MemberManagementLayer from "../layer/MemberManagementLayer";
+import { commonFunction } from "../mixins/commonFunction";
+import SnackBar from "../common/SnackBar";
 export default {
 	components: {
 		PasswordChangeLayer,
 		BosangLayer001,
 		BosangLayer002,
 		MemberManagementLayer,
+		SnackBar,
 	},
 	data: () => ({
+		showSnackBar: false,
+		snackData: "",
 		showPasswordChangeLayer: false, //첨부이미지 보기 레이어
 		showBosangLayer001: false, //첨부이미지 보기 레이어
 		showBosangLayer002: false, //첨부이미지 보기 레이어
 		showMemberManagementLayer: false, //첨부이미지 보기 레이어
-		searchParams: {
-			number: 2, //결합인원(2~5)
-			ratePlanType: 130000, //요금제  가격대
-			lineNumber: 1, //수정하는 결합회선  아이디(1~5)
-			teenagerNumber: 0, //청소년  할인수(0~5)
-			type: "1G", //인터넷(랜선)할인  유형
-			customRatePlanType: [],
-		},
-		numberItems: [2, 3, 4, 5],
-		priceItems: [80000, 90000, 100000, 110000, 130000],
-		teenagerItems: [0, 1, 2, 3, 4, 5],
-		typeItems: ["500M", "1G"],
-		resultDatas: [], //결과값
 	}),
 	methods: {
 		MapOpen(val) {
@@ -254,51 +256,17 @@ export default {
 				"window팝업"
 			);
 		},
-		totalData(type, line) {
-			let plus = "";
-			let i = 0;
-			axios
-				.get("/api/chrg?viewType=" + type, {
-					params: {
-						number: this.searchParams.number, //결합인원(2~5)
-						ratePlanType:
-							type === "line"
-								? this.searchParams.customRatePlanType[line]
-								: this.searchParams.ratePlanType, //요금제  가격대
-						lineNumber: line, //수정하는 결합회선  아이디(1~5)
-						teenagerNumber: this.searchParams.teenagerNumber, //청소년  할인수(0~5)
-						type: this.searchParams.type, //인터넷(랜선)할인  유형
-					},
-				})
-				.then(({ data }) => {
-					console.info(data);
-					if (type === "total") {
-						for (i = 1; i < this.searchParams.ratePlanType; i++) {
-							this.searchParams.customRatePlanType[i] =
-								this.searchParams.ratePlanType;
-						}
-						this.resultDatas = data.data;
-					} else if (type === "line") {
-						for (i = 0; i < this.resultDatas.length; i++) {
-							this.resultDatas[i].lines[line] = data.data[i];
-						}
-					} else if (type === "internet") {
-						for (i = 0; i < this.resultDatas.length; i++) {
-							this.resultDatas[i].internet = data.data[i];
-						}
-					} else if (type === "teenager") {
-						console.info("i");
-						for (i = 0; i < this.resultDatas.length; i++) {
-							this.resultDatas[i].teenager = data.data[i];
-						}
-					}
-				});
+		setSnackData: function (data) {
+			//스낵바 부모액션
+			this.snackData = data;
+			this.showSnackBar = true;
 		},
 	},
 	watch: {},
 	created() {},
 	mounted() {},
 	computed: {},
+	mixins: [commonFunction],
 };
 </script>
 <style scoped>
