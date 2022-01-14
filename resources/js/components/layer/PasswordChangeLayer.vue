a94d1fdea7c6f3069b6db6eb2f593d806ab1aee8
libe9072 <libe90@nate.com>
2022-01-14 09:36:15 +0900

    비밀번호변경

--- resources/js/components/layer/PasswordChangeLayer.vue
+++ resources/js/components/layer/PasswordChangeLayer.vue
@@ -10,34 +10,52 @@
 			:visible="showFullModal"
 			@closed="showFullModal = false"
 			:topProps="topProps"
-			@nextClick="nextClick"
+			@nextClick="submitAction"
 		>
 			<template v-slot:viewContent>
 				<v-container>
-					<!-- 첨부이미지 -->
-					<v-card class="pa-2">
-						<v-text-field
-							placeholder="기존 비밀번호"
-							label="기존 비밀번호"
-							v-model="sm_password"
-							type="password"
-						>
-						</v-text-field>
-						<v-text-field
-							placeholder="신규 비밀번호"
-							label="신규 비밀번호"
-							v-model="sm_password2"
-							type="password"
-						>
-						</v-text-field>
-						<v-text-field
-							placeholder="비밀번호 확인"
-							label="비밀번호 확인"
-							v-model="sm_passwor3"
-							type="password"
-						>
-						</v-text-field>
-					</v-card>
+					<v-form v-model="valid" ref="form">
+						<!-- 첨부이미지 -->
+						<v-card class="pa-2">
+							<v-text-field
+								placeholder="기존 비밀번호"
+								label="기존 비밀번호"
+								v-model="sm_pw"
+								:append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+								:type="show1 ? 'text' : 'password'"
+								@click:append="show1 = !show1"
+								:rules="[
+									rules.required,
+									rules.checkPass(sm_pw, checkedLastPassword),
+								]"
+								@blur="checkPassWord"
+							>
+							</v-text-field>
+							<v-text-field
+								placeholder="신규 비밀번호"
+								label="신규 비밀번호"
+								v-model="new_pw"
+								:append-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'"
+								:type="show2 ? 'text' : 'password'"
+								:rules="[rules.min(new_pw)]"
+								@click:append="show2 = !show2"
+							>
+							</v-text-field>
+							<v-text-field
+								placeholder="비밀번호 확인"
+								label="비밀번호 확인"
+								v-model="confirm_pw"
+								:append-icon="show3 ? 'mdi-eye' : 'mdi-eye-off'"
+								:type="show3 ? 'text' : 'password'"
+								:rules="[
+									rules.min(new_pw),
+									rules.match(confirm_pw, new_pw),
+								]"
+								@click:append="show3 = !show3"
+							>
+							</v-text-field>
+						</v-card>
+					</v-form>
 				</v-container>
 			</template>
 		</ModalLayer>
@@ -47,6 +65,7 @@
 <script>
 import ModalLayer from "../layout/ModalLayer";
 import dataProps from "../../assets/dataProps";
+import axios from "axios";
 import { commonFunction } from "../mixins/commonFunction";
 export default {
 	name: "PasswordChangeLayer",
@@ -76,8 +95,88 @@ export default {
 	},
 	data: () => ({
 		topProps: dataProps.fullModal.PasswordChangeLayer,
+		sm_pw: null,
+		new_pw: null,
+		confirm_pw: null,
+		show1: false,
+		show2: false,
+		show3: false,
+		rules: {
+			required: (value) => !!value || "필수값입니다.",
+			min: function min(data) {
+				//현재 비밀번호 입력값 맞는지 확인 용
+				if (data && typeof data !== "undefined") {
+					return (
+						data.length > 3 ||
+						"변경 비밀번호를 4자리 이상 입력하시기 바랍니다."
+					);
+				}
+				return true;
+			},
+			checkPass: function checkPass(data, last_pass) {
+				//현재 비밀번호 입력값 맞는지 확인 용
+				if (data && typeof data !== "undefined") {
+					return last_pass === true || "현재 비밀번호를 확인하세요.";
+				}
+				return true;
+			},
+			match: function match(data, password) {
+				// 변경할 비밀번호와 변경확인용 비밀번호 체크
+				if (password && typeof password !== "undefined") {
+					return data === password || "변경 비밀번호를 확인하세요.";
+				}
+				return true;
+			},
+		},
+		checkedLastPassword: false, // 현재 비밀번호 입력값 맞는지 확인 용
 	}),
-	methods: {},
+	methods: {
+		checkPassWord() {
+			// 현재 비밀번호 확인
+			if (this.sm_pw) {
+				axios
+					.post("/api/saleman/checkCurrentPassword?_method=PUT", {
+						_token: this.csrfToken,
+						sm_pw: this.sm_pw,
+						params: { type: "mod" },
+					})
+					.then(({ data }) => {
+						this.checkedLastPassword =
+							data.code === 1 ? true : false;
+					});
+			}
+		},
+		submitAction() {
+			this.validate();
+			if (this.valid === true) {
+				axios
+					.post("/api/saleman/changePassword?_method=PUT", {
+						_token: this.csrfToken,
+						new_pw: this.new_pw,
+						params: { type: "mod" },
+					})
+					.then(({ data }) => {
+						localStorage.savePw = this.new_pw;
+						this.$emit(
+							"snackData",
+							this.makeSnakeData(
+								"비밀번호 변경이 완료되었습니다.",
+								"success"
+							)
+						);
+						this.resetPasswordData();
+						this.checkedLastPassword = false;
+						this.$emit("update:showPasswordChangeLayer", false);
+					});
+			}
+		},
+		// 입력한 비밀번호들 초기화
+		resetPasswordData() {
+			this.sm_pw = null;
+			this.new_pw = null;
+			this.confirm_pw = null;
+		},
+	},
 	created() {},
 	watch: {},
 	mixins: [commonFunction],
